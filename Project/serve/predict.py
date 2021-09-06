db91101d6ad1d55ab5ea291bc509c4df8ac3fcdd
newanagata1 <newanagata@gmail.com>
2021-09-06 17:11:59 +0700

    fixed the wrong function name

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -70,7 +70,7 @@ def predict_fn(input_data, model):
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
     
-    data_X,data_len = convert_and_pad_data(model.word_dict, [review_to_words(input_data)])
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
     #data_X = None
     #data_len = None
 
